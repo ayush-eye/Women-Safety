cf583a31279f5684e1eab2d3055cbec775f5cfff
Tanuj Keshattiwar <tanujkeshattiwar@gmail.com>
2026-03-12 09:24:28 +0530

    Added location in map

--- backend/src/controllers/sos.controller.js
+++ backend/src/controllers/sos.controller.js
@@ -1,55 +1,4 @@
-// import twilio from "twilio";
-// import dotenv from "dotenv";
 
-// dotenv.config();
-
-// const client = twilio(
-//   process.env.TWILIO_ACCOUNT_SID,
-//   process.env.TWILIO_AUTH_TOKEN
-// );
-
-// export const calluser = async (req, res) =>
-// {
-//     try
-//     {
-       
-//         const { phone } = req.body;
-//         const name = req.user.name || "The user";
-         
-     
-//         if(!phone)
-//         {
-//             return res.status(400).json({
-//                 success: false,
-//                 message: "Phone number required"
-//             });
-//         }
-
-//         const sms = await client.messages.create({
-//             body: `🚨 SOS ALERT: ${name} may be in danger. Please contact immediately.`,
-//             from: process.env.TWILIO_PHONE,
-//             to: phone
-//         });
-
-//         const call = await client.calls.create({
-//             to: phone,
-//             from: process.env.TWILIO_PHONE,
-//           url: `https://tiana-untransmissible-brent.ngrok-free.dev/voice?name=${encodeURIComponent(name)}`
-//         });
-
-//         return res.status(200).json({
-//             success: true,
-//             callSid: call.sid
-//         });
-//     }
-//     catch(error)
-//     {
-//         return res.status(500).json({
-//             success: false,
-//             error: error.message
-//         });
-//     }
-// };
 
 import twilio from "twilio";
 import dotenv from "dotenv";
@@ -61,6 +10,8 @@ const client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TO
 let contacts = [];
 let currentIndex = 0;
 let userName = "";
+let lat ="";
+let lng = "";
 
 
 const callNextContact = async () =>
@@ -92,6 +43,11 @@ export const calluser = async (req, res) =>
     {
         contacts = req.body.phones;   // array of emergency numbers
         userName = req.user.name || "The user";
+        lat = req.body.lat;
+        lng = req.body.lng;
+
+        const mapLink = `https://maps.google.com/?q=${lat},${lng}`;
+
         currentIndex = 0;
 
         if(!contacts || contacts.length === 0)
@@ -106,7 +62,7 @@ export const calluser = async (req, res) =>
         for(const phone of contacts)
         {
             await client.messages.create({
-                body:`🚨 SOS ALERT: ${userName} may be in danger. Please contact immediately.`,
+                body:`🚨 SOS ALERT: ${userName} may be in danger. Please contact immediately. Current Location of User: ${mapLink}`,
                 from:process.env.TWILIO_PHONE,
                 to:phone
             });
