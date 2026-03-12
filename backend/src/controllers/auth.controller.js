dae5eb1859c338f02171db5b093d6c5b853361ee
Tanuj Keshattiwar <tanujkeshattiwar@gmail.com>
2026-03-12 16:27:52 +0530

    Added gps sms

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -78,7 +78,7 @@ const loginUser = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: false,
-      maxAge: 10 * 24 * 60 * 60 * 1000
+      maxAge: 30 * 24 * 60 * 60 * 1000
     });
 
     const userobj = user.toObject();
